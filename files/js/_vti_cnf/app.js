151276b4446ea942f7e4c5e6f0252ef11b587d45
wetdiver <sxm12345@gmail.com>
2020-11-08 12:59:24 -0500

    Add files via upload

---
+++ files/js/_vti_cnf/app.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DESKTOP-G6ML7DK\\Gabe
+vti_modifiedby:SR|DESKTOP-G6ML7DK\\Gabe
+vti_timelastmodified:TR|08 Nov 2020 17:26:00 -0000
+vti_timecreated:TR|08 Nov 2020 16:23:10 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|index.html
+vti_nexttolasttimemodified:TW|08 Nov 2020 17:25:25 -0000
+vti_cacheddtm:TX|08 Nov 2020 17:26:00 -0000
+vti_filesize:IR|2742
---
+++ files/js/app.js
@@ -0,0 +1,40 @@
+﻿
+data={
+baritem:'w3-bar-item',
+block:'w3-block'
+
+}
+
+links=`<a class="w3-btn w3-bar-item w3-blue" href="#"  title="" >home</a> 
+  <a class="w3-btn w3-bar-item w3-blue w3-margin" href="https://st-maarten-mobile-massage-service.com/about-onsite-treatments.html"  title="about-us" >about massage service</a>
+  <a class="w3-btn w3-bar-item w3-blue w3-margin" href="#four-hand-massage"  title="four-hand-massage" >four hands relaxation</a>
+  <a class="w3-btn w3-bar-item w3-blue" href="#couple-treatments"  title="couple-massage" >couple-massage</a>
+  <a class="w3-btn w3-bar-item w3-blue w3-margin" href="#erotic-massage"  title="couple-massage" >erotic-massage</a>
+  <a class="w3-btn w3-bar-item w3-blue w3-margin " href="#nude-massage"  title="couple-massage" >naturalist-massage</a>
+  <a class="w3-btn w3-bar-item w3-blue w3-margin" href="http://sxmmobilemassage.com/"  title="stmaarten-mobile-massage" >st maarten mobile massage</a>
+`
+
+
+
+pic1=`<img alt='the-emerald-resort-sxm' src='https://scontent-sju1-1.xx.fbcdn.net/v/t1.0-9/124040744_3228457590597241_8462121675058149841_o.jpg?_nc_cat=109&ccb=2&_nc_sid=8bfeb9&_nc_ohc=_SsrPFTUPlsAX_ud4AF&_nc_ht=scontent-sju1-1.xx&oh=c25b799c1aeab1d0d50654188900dad0&oe=5FCECF18' width='100%' />`,
+pic2=`<img alt='orient-villas' src='https://scontent-sju1-1.xx.fbcdn.net/v/t1.0-9/121370654_3157553101021024_2566421069857732228_o.jpg?_nc_cat=110&ccb=2&_nc_sid=8bfeb9&_nc_ohc=f-oT9z7uSxUAX-Xs5a7&_nc_ht=scontent-sju1-1.xx&oh=8cc6672a7801df99760f232c0398c821&oe=5FCC31EF' width='100%' />`,
+pic3=`<img alt='full-body-massage' src='https://scontent-sju1-1.xx.fbcdn.net/v/t1.0-9/120039671_3100607976715537_6567908733445759012_n.jpg?_nc_cat=103&ccb=2&_nc_sid=730e14&_nc_ohc=jwjOJgMOAQMAX80-03x&_nc_ht=scontent-sju1-1.xx&oh=bd0a88e672ca82ff83ee139000434418&oe=5FCE3C0A' width='100%' />`,
+pic4=`<img alt='maho-beach' src='https://scontent-sju1-1.xx.fbcdn.net/v/t1.0-9/106370116_2859229447520059_1795922639513752993_o.jpg?_nc_cat=107&ccb=2&_nc_sid=730e14&_nc_ohc=mdC8XKKpKDMAX_ogJq8&_nc_ht=scontent-sju1-1.xx&oh=721d4f2ec5dbeee572545ecb027f6030&oe=5FCE4A84' width='100%' />`,
+pic5=`<img  alt='saint-martin-couple-massage'src='https://scontent-sju1-1.xx.fbcdn.net/v/t1.0-9/83799025_2519310881511919_2850126708349075456_o.jpg?_nc_cat=101&ccb=2&_nc_sid=730e14&_nc_ohc=MyNVqSEb3TkAX9QPXR4&_nc_ht=scontent-sju1-1.xx&oh=886226e6a7abf41a173ae2246d726bc9&oe=5FCCE6A3' width='100%' />`
+
+
+
+
+document.getElementById("menu").innerHTML=links;
+document.getElementById('pic1').innerHTML=pic1
+document.getElementById('pic2').innerHTML=pic2
+document.getElementById('pic3').innerHTML=pic3
+document.getElementById('pic4').innerHTML=pic4
+document.getElementById('pic5').innerHTML=pic5
+
+
+
+
+
+
+
